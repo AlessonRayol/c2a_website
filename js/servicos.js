f1b715a31199eb359b2e63bf2ac657efd6c3abaa
Vinicius Valente <viniciusvalente@MacBook-Air-de-Vinicius.local>
2019-07-15 22:03:51 -0300

    main design done

--- js/servicos.js
+++
@@ -1,7 +0,0 @@
-let myCarousel = document.getElementById("carouselExampleIndicators");
-myCarousel.carousel("cycle");
-
-myCarousel.addEventListener("click", function() {
-    myCarousel.carousel("pause");
-    alert("asd");
-});
